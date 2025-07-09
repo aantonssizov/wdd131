f39a8e5bc586157919c7666ec0b803e2783df78b
Anton Syzov <aantonssizov@gmail.com>
2025-07-09 18:30:19 -0400

    W02 Learning Activity: Handling DOM Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,26 @@ let favchapInpt = document.getElementById("favchap");
 let addChapBtn = document.getElementById("addchap");
 let chapListElm = document.getElementById("list");
 
-let chapListItemElm = document.createElement("li");
-let chapListItemDeleteBtn = document.createElement("button");
+addChapBtn.addEventListener("click", () => {
+    if (favchapInpt.value.length === 0) {
+        alert("Input field should not be empty");
+        favchapInpt.focus();
+        return;
+    }
 
-chapListItemElm.textContent = favchapInpt.value;
-chapListItemDeleteBtn.textContent = '❌';
-chapListItemElm.appendChild(chapListItemDeleteBtn);
-chapListElm.appendChild(chapListItemElm)
+    let chapListItemElm = document.createElement("li");
+    let chapListItemDeleteBtn = document.createElement("button");
+
+    chapListItemElm.textContent = favchapInpt.value;
+    chapListItemDeleteBtn.textContent = '❌';
+    chapListItemElm.appendChild(chapListItemDeleteBtn);
+    chapListElm.appendChild(chapListItemElm);
+
+    chapListItemDeleteBtn.addEventListener('click', () => {
+        chapListElm.removeChild(chapListItemElm)
+        favchapInpt.focus();
+    });
+
+    favchapInpt.value = "";
+    favchapInpt.focus();
+});
